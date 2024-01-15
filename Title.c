a3784c81f9e7e35f1d5805ffcb75883a8707566d
daacyberian <syeddawood463@gmai.com>
2024-01-15 00:04:07 -0800

    ok

--- Title.c
+++ Title.c
@@ -117,7 +117,7 @@ void Rules()
 }
 void limitations()
 {
-    printf("\t\t\t\t\t\t\t LIMITED PERSONALITIES\n");
-    printf("\t\t\t\t\t\t\t\t 1)Nawaz Sharif \n\t\t\t\t\t\t\t\t2)Shabaz Sharif\n\t\t\t\t\t\t\t\t3)Altaf Hussain\n\t\t\t\t\t\t\t\t4)Imran Khan\n\t\t\t\t\t\t\t\t5)Sheikh Rasheed\n\t\t\t\t\t\t\t\t6)Bilawal Bhutto\n\t\t\t\t\t\t\t\t7)Benazir Bhutto\n\t\t\t\t\t\t\t\t8)Maryam Nawaz\n\t\t\t\t\t\t\t\t9)Firdous Ashiq Awan\n");
+    printf("\t\t\t\t\t\t\t      LIMITED PERSONALITIES\n");
+    printf("\t\t\t\t\t\t\t\t1)Nawaz Sharif \n\t\t\t\t\t\t\t\t2)Shabaz Sharif\n\t\t\t\t\t\t\t\t3)Altaf Hussain\n\t\t\t\t\t\t\t\t4)Imran Khan\n\t\t\t\t\t\t\t\t5)Sheikh Rasheed\n\t\t\t\t\t\t\t\t6)Bilawal Bhutto\n\t\t\t\t\t\t\t\t7)Benazir Bhutto\n\t\t\t\t\t\t\t\t8)Maryam Nawaz\n\t\t\t\t\t\t\t\t9)Firdous Ashiq Awan\n");
 
 }
